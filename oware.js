97f5b96f4b01ac76e90068d52d73905b8229b4fd
Edgar Torre <up201906573@fe.up.pt>
2021-11-09 10:26:30 +0000

    bla

--- oware.js
+++ oware.js
@@ -4,6 +4,7 @@ var score = [0,0];
 
 var playerCavityNumber = 6;
 var initialSeedsPerCavity = 4;
+var totalCavities;
 var roundCounter = 0;
 
 var isPlayer1Turn;
@@ -17,22 +18,70 @@ function gameSetup() {
     // PvP vs PvE setup
     // If PvE, AI level setup
 
-    for (var i = 0; i < playerCavityNumber * 2; i++) {
+    totalCavities = playerCavityNumber*2;
+
+    for (var i = 0; i < totalCavities; i++) {
         board.push(initialSeedsPerCavity);
     }
+
 
     isPlayer1Turn = isPlayer1Starting;
 }
 
-function selectCavity(){
-    console.log('Choosea number between 1 and ' + playerCavityNumber*2);
-    return 5;
+function verifyScoring() {
+    
 }
 
-function executePlay(cavityIndex) {
-    for (var i = board[cavityIndex]; i > 0; i--) {
-        
+function selectCavity() {
+    
+    console.log('Choose a number between ' + (isPlayer1Turn ? 0 : playerCavityNumber - 1) + 
+                ' and ' + (isPlayer1Turn ? playerCavityNumber : totalCavities - 1));
+
+    return 5; //TODO
+    var chosenCavity;
+    // read input to chosen cavity
+    
+    while (true) {
+        if ((isPlayer1 && chosenCavity >= 0 && chosenCavity < playerCavityNumber) ||
+            (!isPlayer1 && chosenCavity >= playerCavityNumber && chosenCavity < totalCavities)) 
+                return chosenCavity;
     }
+}
+
+function executePlay(cavityIndex) { //TODO
+    var seeds = board[cavityIndex];
+    
+    var loopCounter = 0;
+    var didBoardSideSwitch = false;
+
+    for (var index = cavityIndex; index < cavityIndex + seeds + loopCounter; index++) {
+
+        if (index % totalCavities == 0) loopCounter++;
+
+        var currentCavity = index % totalCavities;
+        var isOpositePlayerFirstCavity = (currentCavity == (isPlayer1Turn ? playerCavityNumber-1 : totalCavities-1));
+
+        if (isOpositePlayerFirstCavity && didBoardSideSwitch) {
+            didBoardSideSwitch = false;
+            score[(isPlayer1Turn ? 0 : 1)]++;
+            continue;
+        }
+
+
+
+    }
+    /*
+    for (var i = 1; i < seeds + loopCounter + 1; i++) {
+        if (i == totalCavities) loopCounter++;
+
+        board[(i + cavityIndex) % totalCavities]++;
+    }
+    */
+
+
+    board[cavityIndex] = 0;
+
+    verifyScoring((i + cavityIndex - 1) % totalCavities);
 }
 
 function isGameFinished() {
@@ -61,10 +110,18 @@ function showBoard() {
     console.log('Eventually the board will be shown...');
 }
 
-function main() {
+function viewBoard() {
+    for (var i = 0; i < totalCavities; i++) {
+        console.log(board[i] + " at index " + i);
+    }
 
+    console.log("\n\n\n");
+}
+
+function main() {
     gameSetup();
 
+    /*
     while(true){
         showBoard();
         if (isGameFinished()) return 0;
@@ -74,6 +131,8 @@ function main() {
         isPlayer1Turn = !isPlayer1Turn;
         roundCounter++;
     }
+    */
+
 }
 
 main();
